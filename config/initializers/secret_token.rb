568b3c554e4f478f193e0558260e6d292223c349
Max McCarty <max.mccarty@verizon.net>
2013-01-26 10:37:20 -0500

    initial commit woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemesher::Application.config.secret_token = '42158efcf070e00afa16d909e90ef2538a638d41ea191bd3d30421560849512f1ce99f896bd1452d2ba888662998948d3d67a1c960afc0929d14a256f500a81b'
