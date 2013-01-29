1c25b487a89313886ceab041b1a44435319bb021
Max McCarty <max.mccarty@verizon.net>
2013-01-29 16:29:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemesher::Application.config.secret_token = '8b0a40e6ee5c61da3fe7ec0b92237378ded3f6fab8ce1a4e24a3400efcbf8f1cc8dc856d6f0d0cbaa25034a5291d6e68f9977cd4d60352f356a12b6bb3ad1b71'
